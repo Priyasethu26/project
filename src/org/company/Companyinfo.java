7218b4cad05be7a3ba8a1ed539b8b489c8164ca1
Priyasethu26 <ssphpriya@gmail.com>
2021-12-28 10:39:08 +0530

    structure

---
+++ src/org/company/Companyinfo.java
@@ -0,0 +1,27 @@
+package org.company;
+
+public class Companyinfo {
+private void companyname() {
+	System.out.println("cts");
+	}
+private void companyid() {
+	System.out.println("232");
+}
+private void companyaddress()
+{
+	System.out.println("aaa");
+}
+public static void main (String[]args) {
+	Companyinfo zz=new Companyinfo();
+	zz.companyname();
+	zz.companyid();
+	zz.companyaddress();
+			}
+	
+
+}
+
+	
+
+
+
